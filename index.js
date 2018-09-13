24575411f8e8c1ad036a5e77256a147cc3a4bfe9
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-13 11:27:02 -0700

    get ready for github

--- index.js
+++ index.js
@@ -14,20 +14,31 @@ const app = express();
 // Set up middleware
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+/* we use cors on the entire app for convienence, but basically needed any time
+ * a route is connecting with the front end */
 app.use(cors());
+// do not accept "weird" data, also prevents a potential source of server crash
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// helper function: this allows our server to parse incoming token from client
+function fromRequest(req){
+  if(req.body.headers.Authorization &&
+    req.body.headers.Authorization.split(' ')[0] === 'Bearer'){
+    return req.body.headers.Authorization.split(' ')[1];
+  }
+  return null;
+}
+
+
 // Controllers
+/* NOTE here we use the JWT middleware to protect relevant routes
+ * you can see how we say "unless" to specify that we dont want it to run on
+ * those, WARN it has access to request because it is middleware
+ * so it has (req, res, next) implicitly */
 app.use('/auth', expressJWT({
   secret: process.env.JWT_SECRET,
-  getToken: function fromRequest(req){
-    if(req.body.headers.Authorization &&
-      req.body.headers.Authorization.split(' ')[0] === 'Bearer'){
-      return req.body.headers.Authorization.split(' ')[1];
-    }
-    return null;
-  }
+  getToken: fromRequest
 }).unless({
   path: [
     { url: '/auth/login', methods: ['POST'] },
@@ -35,8 +46,10 @@ app.use('/auth', expressJWT({
   ]
 }), require('./controllers/auth'));
 
+// catch all route, ideally don't get here unless mistake made
+// NOTE this should not be user error, this should be front-end dev error
 app.get('*', function(req, res, next) {
-	res.send({ message: 'Unknown Route' });
+  res.send({ message: 'Unknown Route' });
 });
 
 app.listen(process.env.PORT || 3000);
--- models/user.js
+++ models/user.js
@@ -24,6 +24,7 @@ var userSchema = new mongoose.Schema({
 });
 
 // Override 'toJSON' to prevent the password from being returned with the user
+// WARN toJSON is a method for the user model, so it's something from Mongoose
 userSchema.set('toJSON', {
   transform: function(doc, user, options) {
     var returnJson = {
@@ -39,6 +40,9 @@ userSchema.methods.authenticated = function(password) {
   return bcrypt.compareSync(password, this.password);
 }
 
+/* NOTE next is pausing the function and hashSync is pausing the db save
+ * next prevents the next function from executing and hashSync prevents the
+ * next call from being sent too early */
 // Mongoose's version of a beforeCreate hook
 userSchema.pre('save', function(next) {
   var hash = bcrypt.hashSync(this.password, 10);
